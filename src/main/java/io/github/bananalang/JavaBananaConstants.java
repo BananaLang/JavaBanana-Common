606ae8e1950adaf8d7eb6ac2fb7cdb4b1baf0d73
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-12-11 14:50:30 -0600

    Create JavaBananaConstants.java

---
+++ src/main/java/io/github/bananalang/JavaBananaConstants.java
@@ -0,0 +1,9 @@
+package io.github.bananalang;
+
+public final class JavaBananaConstants {
+    public static final boolean DEBUG = Boolean.getBoolean("bananalang.debug");
+
+    // Disable instatiation
+    private JavaBananaConstants() {
+    }
+}
